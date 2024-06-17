4564c8802a9b165114906d7c43ca73266d7e86c1
Diana Kozachenko <kozachenkodiana03@mail.ru>
2024-06-17 20:44:23 +0300

    refactor(all): move template to html files, folder restructring and barrel files

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,8 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-import { HousingLocation } from '../housinglocation';
-import { HousingService } from '../housing.service';
+import { HousingLocation } from '../../models';
+import { HousingService } from '../../services';
 
 @Component({
   selector: 'app-home',
@@ -11,21 +11,8 @@ import { HousingService } from '../housing.service';
     CommonModule,
     HousingLocationComponent
   ],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" #filter>
-        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
-      </form>
-    </section>
-    <section class="results">
-      <app-housing-location
-        *ngFor="let housingLocation of filteredLocationList"
-        [housingLocation]="housingLocation">
-      </app-housing-location>
-    </section>
-  `,
-  styleUrls: ['./home.component.css'],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 
 export class HomeComponent {

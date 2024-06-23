0a830b92f42e06ee171bb616a170709734dc34a9
Diana Kozachenko <kozachenkodiana03@mail.ru>
2024-06-23 21:43:00 +0300

    feat(rent-houses-transloco): add translations to service

--- apps/rent-houses-transloco/src/components/home/home.component.ts
+++ apps/rent-houses-transloco/src/components/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { TranslocoDirective, TranslocoService } from '@jsverse/transloco';
 import { Title } from '@angular/platform-browser';
+import { Subscription, filter } from 'rxjs';
 import { HousingLocation } from '../../models';
 import { HousingService } from '../../services';
 
@@ -13,7 +14,9 @@ import { HousingService } from '../../services';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private langSub!: Subscription;
+
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
   filteredLocationList: HousingLocation[] = [];
@@ -21,15 +24,18 @@ export class HomeComponent implements OnInit {
   private readonly titleService = inject(Title);
   private readonly translocoService = inject(TranslocoService);
 
-  constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
-  }
-
   ngOnInit(): void {
-    this.translocoService
+    this.langSub = this.translocoService
       .selectTranslate('home-page-title')
       .subscribe((value: string) => this.titleService.setTitle(value));
+
+    const eventSub = this.translocoService.events$
+      .pipe(filter((e) => e.type === 'translationLoadSuccess'))
+      .subscribe(() => {
+        this.housingLocationList = this.housingService.getAllHousingLocations();
+        this.filteredLocationList = this.housingLocationList;
+      });
+    this.langSub.add(eventSub);
   }
 
   filterResults(text: string) {
@@ -42,5 +48,9 @@ export class HomeComponent implements OnInit {
       (housingLocation) =>
         housingLocation?.city.toLowerCase().includes(text.toLowerCase())
     );
+  }
+
+  ngOnDestroy(): void {
+    this.langSub.unsubscribe();
   }
 }
